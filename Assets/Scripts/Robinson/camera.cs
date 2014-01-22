30cd9542aa3445e73e8ab212d7a6ca5da3effa45
Jonathan Robinson <robij308@coyote.csusb.edu>
2014-01-21 17:12:05 -0800

    Completed the first assignment.

---
+++ Assets/Scripts/Robinson/camera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+	private Transform obj;
+	public Vector3 newRotation = new Vector3();
+	// Use this for initialization
+	void Start () {
+		obj = GameObject.FindGameObjectWithTag ("Cube").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newRotation.Set (obj.rotation.x + 10f, obj.rotation.y + 10f, 0f);
+		obj.Rotate (newRotation);
+	}
+}
---
+++ Assets/Scripts/Robinson/spinCube.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spinCube : MonoBehaviour 
+{
+	private Transform obj;
+	public Vector3 newRotation = new Vector3();
+	// Use this for initialization
+	void Start () 
+	{
+		obj = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		newRotation.Set (transform.rotation.x + 10f, transform.rotation.y + 10f, 0f);
+		obj.Rotate (newRotation);
+	}
+}
