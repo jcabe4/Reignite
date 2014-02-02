1bd2bfe13faf7e0d768882a56e7e2d09e7fc02e0
mcswedo <mcswedo@gmail.com>
2014-02-01 22:12:02 -0800

    Mouse movement test. Set up polygon collider.

---
+++ Assets/Scripts/Michael/MovementController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour 
+{
+	private PolygonCollider2D backgroundCollider;
+	public Vector3 futurePosition;
+	public float moveSpeed = .075f;
+
+	void Start() 
+	{
+		backgroundCollider = GameObject.FindGameObjectWithTag("Environment").GetComponent<PolygonCollider2D>();
+		futurePosition = transform.position;
+	}
+	
+	void Update() 
+	{
+		if(Input.GetMouseButton(0))
+		{
+			futurePosition = Input.mousePosition;
+			futurePosition = Camera.main.ScreenToWorldPoint(futurePosition);
+			futurePosition.z = transform.position.z;
+		}
+
+		transform.position = Vector3.MoveTowards(transform.position, futurePosition, moveSpeed);
+	}
+}
