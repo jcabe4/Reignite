a70b5669e68cf55468e08180109d1ec42aeebc4d
mcswedo <mcswedo@gmail.com>
2014-02-03 13:06:28 -0800

    More work

--- Assets/Scripts/Michael/KeyboardInput.cs
+++ Assets/Scripts/Michael/KeyboardInput.cs
@@ -5,13 +5,11 @@ public class KeyboardInput : MonoBehaviour
 {
 	
 	Vector3 futurePosition;
-	Vector3 currentPosition;
 
 	float moveSpeed = .075f;
 
 	void Start() 
 	{
-		currentPosition = transform.position;
 		futurePosition = transform.position;
 	}
 
@@ -22,9 +20,52 @@ public class KeyboardInput : MonoBehaviour
 
 	void HandleKeyboard()
 	{
-		if(Input.GetAxis("Horizontal") > 1)
+		if(Input.GetAxis("Horizontal") > 0)
 		{
-
+			futurePosition.x += moveSpeed;
+			if(IsColliding() == false)
+			{
+				this.transform.position = futurePosition;
+			}
+		}
+		if(Input.GetAxis("Horizontal") < 0)
+		{
+			futurePosition.x -= moveSpeed;
+			if(IsColliding() == false)
+			{
+				this.transform.position = futurePosition;
+			}
+		}
+		if(Input.GetAxis("Vertical") > 0)
+		{
+			futurePosition.y += moveSpeed;
+			if(IsColliding() == false)
+			{
+				this.transform.position = futurePosition;
+			}
+		}
+		if(Input.GetAxis("Vertical") < 0)
+		{
+			futurePosition.y -= moveSpeed;
+			if(IsColliding() == false)
+			{
+				this.transform.position = futurePosition;
+			}
 		}
 	}
+	
+	bool IsColliding()
+	{
+		GameObject[] obj = GameObject.FindGameObjectsWithTag("Obstacle");
+
+		for (int i = 0; i <= obj.Length; i++)
+		{
+			if(obj[i].collider.bounds.Contains(collider.bounds.center) == false)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
