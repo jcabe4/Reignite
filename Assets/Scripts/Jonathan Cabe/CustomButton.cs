bb1864b4ffb334e983795ec6ef9775d746c749f6
Jonathan Cabe <jcabe4@gmail.com>
2014-02-09 12:47:54 -0800

    Rhythm Section "Finished"

---
+++ Assets/Scripts/Jonathan Cabe/CustomButton.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomButton : MonoBehaviour 
+{	
+	public enum buttonType {Start, Continue, Options, Exit};
+	public buttonType type;
+	public UIPanel togglePanel;
+	public UIPanel optionsPanel;
+	public UISprite buttonSprite;
+
+	public string hoverSprite = "Button - Icon (Hover)";
+	public string pushedSprite = "Button - Icon (Active)";
+	public string normalSprite = "Button - Icon";
+	public string activeSprite = "Button - Icon (Active)";
+	
+	private bool bIsSelected;
+
+	void Awake()
+	{
+		if (!buttonSprite)
+		{
+			buttonSprite = gameObject.GetComponent<UISprite>();
+		}
+	}
+
+	void Start()
+	{
+		if (togglePanel)
+		{
+			togglePanel.alpha = 1f;
+		}
+	}
+
+	void Update()
+	{
+		if (togglePanel && Input.GetKeyUp(KeyCode.Return))
+		{
+			togglePanel.alpha = 0f;
+		}
+	}
+
+	void OnPress (bool isPressed)
+	{
+		if (isPressed)
+		{
+			buttonSprite.spriteName = pushedSprite;
+		}
+		else
+		{
+			buttonSprite.spriteName = normalSprite;
+
+			if (type == buttonType.Start)
+			{
+				togglePanel.alpha = 0f;
+				RhythmGame.Instance.BeginGame();
+			}
+			else if (type == buttonType.Continue)
+			{
+				RhythmInput.KeyPress(RhythmInput.Instance.options);
+			}
+		}
+	}
+	
+	void OnHover (bool isOver)
+	{
+		if (isOver)
+		{
+			buttonSprite.spriteName = hoverSprite;
+		}
+		else
+		{
+			buttonSprite.spriteName = normalSprite;
+		}
+	}
+}
--- Assets/Scripts/Jonathan Cabe/RhythmGame.cs
+++ Assets/Scripts/Jonathan Cabe/RhythmGame.cs
@@ -12,24 +12,35 @@ public class RhythmGame : MonoBehaviour
 
 	struct Multiplier
 	{
-		const float bad = 1.25f;
-		const float good = 2f;
-		const float perfect = 4f;
+		public const float bad = 1.25f;
+		public const float good = 2f;
+		public const float perfect = 4f;
 	}
 
 	struct PointValue
 	{
-		const float initial = 100f;
-		const float hold = 10f;
+		public const float initial = 100f;
+		public const float hold = 10f;
+	}
+
+	public static RhythmGame Instance
+	{
+		get
+		{
+			return instance;
+		}
 	}
 
 	public string song;
 	public UISprite beatBar;
 	public UILabel scoreLabel;
+	public UIPanel optionsPanel;
 	public UISlider songProgressBar;
+	public Object labelPrefab;
 	public Object singlePrefab;
 	public Object doublePrefab;
 	public Transform noteParent;
+	public Transform modifierParent;
 	public AudioClip [] songDatabase;
 	public UIAnchor [] spawnPositions;
 	
@@ -37,11 +48,10 @@ public class RhythmGame : MonoBehaviour
 	private float noteWidth;
 	private RhythmNote rNote;
 	private Note.NoteColor color;
-	private PointValue pointValue;
-	private Multiplier multiplier;
 
 	private int score = 0;
 	private int noteIndex = 0;
+	private int hoverBonus = 0;
 	private int notesSpawned = 0;
 	private float deltaTime = 0f;
 	private float timeLapsed = 0f;
@@ -49,15 +59,21 @@ public class RhythmGame : MonoBehaviour
 	private float lengthRatio = 1f;
 	private float moveDistance = 0f;
 	private float totalNoteTime = 0f;
+	private bool bMissed = false;
 	private bool bPlaySong = false;
+	private bool bBeginGame = false;
 	private bool bEarnedPoints = false;
+	private bool bCanEarnPoints = true;
 	private Color32 defaultColor = new Color32(0, 200, 255, 150);
 	
 	private string path;
 	private RhythmInput rInput;
+	private UILabel modifierLabel;
 	private AudioSource musicSource;
 	private Note currentNote = new Note();
 	private List<Note> notes = new List<Note>();
+	
+	private static RhythmGame instance;
 	
 	void Start()
 	{
@@ -68,36 +84,55 @@ public class RhythmGame : MonoBehaviour
 		CalculateLength();
 		timeOffset = 1f / lengthRatio;
 		timeLapsed -= timeOffset;
+
+		instance = this;
 	}
 
 	void OnEnable()
 	{
-		RhythmInput.KeyPress += PauseGame;
+		RhythmInput.KeyPress += KeyCommand;
 		RhythmInput.ColorPress += AddScore;
 		RhythmInput.ColorPress += ChangeBeatBar;
 	}
 
 	void OnDisable()
 	{
-		RhythmInput.KeyPress -= PauseGame;
+		RhythmInput.KeyPress -= KeyCommand;
 		RhythmInput.ColorPress -= AddScore;
 		RhythmInput.ColorPress -= ChangeBeatBar;
 	}
 	
 	void Update()
 	{
-		if (bPlaySong)
+		if (bBeginGame && bPlaySong && timeLapsed < audio.clip.length)
 		{
-			deltaTime = Time.deltaTime;
+			if (timeLapsed < 0f)
+			{
+				deltaTime = Time.deltaTime;
+			}
+			else
+			{
+				if (!audio.isPlaying && timeLapsed < totalNoteTime)
+				{
+					audio.Play();
+				}
+
+				deltaTime = audio.time - timeLapsed;
+			}
+			
 			timeLapsed += deltaTime;
-			moveDistance = lengthRatio * Time.deltaTime;
+			moveDistance = lengthRatio * deltaTime;
 
 			CreateNotes();
-			AddCursorScore();
+			CheckCurrentNote();
 
 			if (Scroll != null)
 			{
 				Scroll(moveDistance);
+			}
+			if (rInput.GetLiftUp())
+			{
+				bCanEarnPoints = true;
 			}
 		}
 
@@ -109,36 +144,210 @@ public class RhythmGame : MonoBehaviour
 		if (!Input.anyKey)
 		{
 			beatBar.color = defaultColor;
+
+			if (audio.volume > .1f && currentNote.color != Note.NoteColor.Pause)
+			{
+				audio.volume -= Time.deltaTime * .5f;
+			}
 		}
 	}
 
-	public void AddScore(Note.NoteColor colorPressed)
+	public void BeginGame()
+	{
+		if (!bBeginGame)
+		{
+			bPlaySong = true;
+			bBeginGame = true;
+		}
+	}
+
+	private void AddScore(Note.NoteColor colorPressed)
 	{
 		if (bPlaySong)
-		{
-			if (currentNote.endTotal < timeLapsed)
+		{			
+			if (rInput.CheckHover())
 			{
-				for (int i = 0; i < notes.Count; i++)
-				{
-					if (notes[i].startTotal < timeLapsed && notes[i].endTotal > timeLapsed)
-					{
-						currentNote = notes[i];
-						break;
-					}
-				}
-			}
-
-			if (currentNote.color == colorPressed)
-			{
-				Debug.Log("ADD SCORE!");
+				hoverBonus = 2;
 			}
 			else
 			{
-				Debug.Log("NO SCORE!");
+				hoverBonus = 1;
+			}
+
+			if (currentNote.color == Note.NoteColor.Pause)
+			{
+				bCanEarnPoints = false;
+			}
+
+			if (currentNote.color == colorPressed && colorPressed != Note.NoteColor.Pause)
+			{
+				if (bCanEarnPoints)
+				{
+					if (!bEarnedPoints)
+					{
+						if (timeLapsed >= currentNote.startTotal &&
+						    timeLapsed < (currentNote.startTotal + currentNote.noteDuration * .125f))
+						{
+							score += (int)(PointValue.initial * Multiplier.perfect * hoverBonus);
+							SpawnText("perfect");
+						}
+						else if (timeLapsed >= currentNote.startTotal && 
+						         timeLapsed < (currentNote.startTotal + currentNote.noteDuration * .25f))
+						{
+							score += (int)(PointValue.initial * Multiplier.good * hoverBonus);
+							SpawnText("good");
+						}
+						else if (timeLapsed >= currentNote.startTotal && 
+						         timeLapsed < (currentNote.startTotal + currentNote.noteDuration * .5f))
+						{
+							score += (int)(PointValue.initial * Multiplier.bad * hoverBonus);
+							SpawnText("bad");
+						}
+						else
+						{
+							score += (int)(PointValue.initial * hoverBonus);
+							SpawnText("miss");
+						}
+
+						bEarnedPoints = true;
+					}
+					else
+					{
+						score +=(int)(PointValue.hold * hoverBonus);
+					}
+
+					if (rInput.CheckHover())
+					{
+						audio.volume += Time.deltaTime * .75f;
+					}
+					else if (audio.volume < .4f)
+					{
+						audio.volume += Time.deltaTime * .5f;
+					}
+				}
+				else
+				{
+					if (audio.volume > .1f)
+					{
+						audio.volume -= Time.deltaTime * .5f;
+					}
+				}
+			}
+			else if (currentNote.color != Note.NoteColor.Pause)
+			{
+				if (audio.volume > .1f)
+				{
+					audio.volume -= Time.deltaTime * .5f;
+				}
+
+				if (!bEarnedPoints && !bMissed)
+				{
+					SpawnText("miss");
+					StartCoroutine("Delay", .5f);
+				}
+			}
+			
+			scoreLabel.text = score.ToString("0000000000");
+		}
+	}
+
+	private void CheckCurrentNote()
+	{
+		if (currentNote.endTotal < timeLapsed)
+		{
+			for (int i = 0; i < notes.Count; i++)
+			{
+				if (notes[i].startTotal < timeLapsed && notes[i].endTotal > timeLapsed)
+				{
+					currentNote = notes[i];
+					bEarnedPoints = false;
+
+					if (!bMissed)
+					{
+						StartCoroutine("Delay", .5f);
+					}
+
+					break;
+				}
 			}
 		}
 	}
 
+	private void SpawnText(string Text)
+	{
+		switch (Text)
+		{
+			case "miss":
+			{
+				GameObject text = (GameObject)Instantiate(labelPrefab, rInput.cursor.transform.position, Quaternion.identity);
+				text.transform.parent = modifierParent.transform;
+				modifierLabel = text.GetComponent<UILabel>();
+
+				modifierLabel.text = "miss";
+				modifierLabel.color = Color.white;
+				modifierLabel.depth = 5;
+
+				text.rigidbody.AddForce(Vector3.up * 100f);
+
+				DestroyObject(text, 1f);
+				
+				break;
+			}
+			case "bad":
+			{
+				GameObject text = (GameObject)Instantiate(labelPrefab, rInput.cursor.transform.position, Quaternion.identity);
+				text.transform.parent = modifierParent.transform;
+				modifierLabel = text.GetComponent<UILabel>();
+
+				modifierLabel.text = "bad";
+				modifierLabel.color = Color.red;
+				modifierLabel.depth = 5;
+
+				text.rigidbody.AddForce(Vector3.up * 100f);
+
+				DestroyObject(text, 1f);
+				
+				break;
+			}
+			case "good":
+			{
+				GameObject text = (GameObject)Instantiate(labelPrefab, rInput.cursor.transform.position, Quaternion.identity);
+				text.transform.parent = modifierParent.transform;
+				modifierLabel = text.GetComponent<UILabel>();
+
+				modifierLabel.text = "GOOD";
+				modifierLabel.color = Color.green;
+				modifierLabel.depth = 5;
+
+				text.rigidbody.AddForce(Vector3.up * 100f);
+
+				DestroyObject(text, 1f);
+				
+				break;
+			}
+			case "perfect":
+			{
+				GameObject text = (GameObject)Instantiate(labelPrefab, rInput.cursor.transform.position, Quaternion.identity);
+				text.transform.parent = modifierParent.transform;
+				modifierLabel = text.GetComponent<UILabel>();
+
+				modifierLabel.text = "PERFECT!";
+				modifierLabel.color = Color.yellow;
+				modifierLabel.depth = 5;
+
+				text.rigidbody.AddForce(Vector3.up * 100f);
+
+				DestroyObject(text, 1f);
+				
+				break;
+			}
+			default:
+			{
+				Debug.Log("Spawn Text: No Case For Argument!");
+				break;
+			}
+		}
+	}
 	private void ChangeBeatBar(Note.NoteColor colorPressed)
 	{
 		if (beatBar)
@@ -168,20 +377,38 @@ public class RhythmGame : MonoBehaviour
 		}
 	}
 
-	private void PauseGame(KeyCode key)
+	private void KeyCommand(KeyCode key)
 	{
-		if (rInput.options == key)
+		if (rInput.confirm == key)
 		{
-			bPlaySong = !bPlaySong;
+			if (!bBeginGame && optionsPanel.alpha == 0f)
+			{
+				bPlaySong = true;
+				bBeginGame = true;
+			}
 		}
-	}
-	private void AddCursorScore()
-	{
-		if (rInput.CheckHover())
+		else if (rInput.options == key)
 		{
-			score += 1;
+			if (bBeginGame)
+			{
+				bPlaySong = !bPlaySong;
 
-			scoreLabel.text = score.ToString("0000000000");
+				if (!bPlaySong)
+				{
+					audio.Pause();
+					optionsPanel.alpha = 1f;
+				}
+				else
+				{
+					optionsPanel.alpha = 0f;
+
+					if (timeLapsed > 0f)
+					{
+						audio.Play();
+						
+					}
+				}
+			}
 		}
 	}
 
@@ -197,33 +424,33 @@ public class RhythmGame : MonoBehaviour
 				
 				switch(notes[noteIndex].color)
 				{
-				case Note.NoteColor.Blue:
-				case Note.NoteColor.Yellow:
-				case Note.NoteColor.Green:
-				case Note.NoteColor.Red:
-				{
-					GameObject newNote = (GameObject)Instantiate(singlePrefab);
-					newNote.transform.parent = noteParent.transform;
-					newNote.transform.localScale = Vector3.one;
-					newNote.name = notesSpawned.ToString() + ": Note (" + notes[noteIndex].color.ToString() + ")";
-					
-					rNote = newNote.GetComponent<RhythmNote>();
-					rNote.Init(pos, color, noteWidth, gameObject);
-					notesSpawned++;
-					break;
-				}
-				default:
-				{
-					GameObject newNote = (GameObject)Instantiate(doublePrefab);
-					newNote.transform.parent = noteParent.transform;
-					newNote.transform.localScale = Vector3.one;
-					newNote.name = notesSpawned.ToString() + ": Note (" + notes[noteIndex].color.ToString() + ")";
-					
-					rNote = newNote.GetComponent<RhythmNote>();
-					rNote.Init(pos, color, noteWidth, gameObject);
-					notesSpawned++;
-					break;
-				}
+					case Note.NoteColor.Blue:
+					case Note.NoteColor.Yellow:
+					case Note.NoteColor.Green:
+					case Note.NoteColor.Red:
+					{
+						GameObject newNote = (GameObject)Instantiate(singlePrefab);
+						newNote.transform.parent = noteParent.transform;
+						newNote.transform.localScale = Vector3.one;
+						newNote.name = notesSpawned.ToString() + ": Note (" + notes[noteIndex].color.ToString() + ")";
+						
+						rNote = newNote.GetComponent<RhythmNote>();
+						rNote.Init(pos, color, noteWidth, gameObject);
+						notesSpawned++;
+						break;
+					}
+					default:
+					{
+						GameObject newNote = (GameObject)Instantiate(doublePrefab);
+						newNote.transform.parent = noteParent.transform;
+						newNote.transform.localScale = Vector3.one;
+						newNote.name = notesSpawned.ToString() + ": Note (" + notes[noteIndex].color.ToString() + ")";
+						
+						rNote = newNote.GetComponent<RhythmNote>();
+						rNote.Init(pos, color, noteWidth, gameObject);
+						notesSpawned++;
+						break;
+					}
 				}
 				
 				noteIndex++;
@@ -269,17 +496,13 @@ public class RhythmGame : MonoBehaviour
 		else
 		{
 			Debug.Log("Song Clip Not Found!!");
-			//return;
+			return;
 		}
 		
 		if (!File.Exists(path))
 		{
 			Debug.Log(songName + ".xml Not Found @ " + path);
 			return;
-		}
-		else
-		{
-			Debug.Log("Loading " + songName + ".xml @ \n" + path);
 		}
 		
 		TextAsset asset = (TextAsset)Resources.Load(resourcePath);
@@ -358,5 +581,12 @@ public class RhythmGame : MonoBehaviour
 				}
 			}
 		}
+	}
+
+	IEnumerator Delay(float time)
+	{
+		bMissed = true;
+		yield return new WaitForSeconds(time);
+		bMissed = false;
 	}
 }
