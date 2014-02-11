b0a2e7765e72656d56b5cc36acec7c30c76e20e8
mcswedo <mcswedo@Michaels-MacBook-Pro.local>
2014-02-10 20:28:39 -0800

    Fixed collider issue in rhythm section.

--- Assets/Scripts/Jonathan Cabe/ResizeCollider.cs
+++ Assets/Scripts/Jonathan Cabe/ResizeCollider.cs
@@ -7,6 +7,7 @@ public class ResizeCollider : MonoBehaviour
 	public enum resizeType {Horizontal, Vertical, Both};
 	public resizeType type;
 
+	public bool doubleBar = false;
 	public UIWidget target;
 	public Vector2 relativeSize = Vector2.one;
 	private Vector2 size;
@@ -18,12 +19,21 @@ public class ResizeCollider : MonoBehaviour
 	{
 		widget = gameObject.GetComponent<UIWidget>();
 		bCollider = gameObject.GetComponent<BoxCollider>();
-
+	
 		if (widget)
 		{
 			if (target && (type == resizeType.Both || type == resizeType.Horizontal))
 			{
 				widget.width = target.width;
+
+				if (doubleBar)
+				{
+					widget.height = target.height * 2;
+				}
+				else
+				{
+					widget.height = target.height;
+				}
 			}
 			if (target && (type == resizeType.Both || type == resizeType.Vertical))
 			{
