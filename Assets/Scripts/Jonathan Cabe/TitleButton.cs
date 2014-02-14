f4f0701b3ffd3c8b44bc1e729cd2327641db4a81
Jonathan Cabe <jcabe4@gmail.com>
2014-02-13 16:31:06 -0800

    Title Screen

---
+++ Assets/Scripts/Jonathan Cabe/TitleButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleButton : MonoBehaviour 
+{
+	private UISprite sprite;
+
+	void Start()
+	{
+		sprite = gameObject.GetComponent<UISprite>();
+
+		sprite.alpha = 0f;
+	}
+
+	void OnHover(bool hover)
+	{
+		if (hover)
+		{
+			sprite.alpha = 1f;
+		}
+		else
+		{
+			sprite.alpha = 0f;
+		}
+	}
+}
