7c0540f02ecbd8da96007d07c5dd5454e61faae7
Jonathan Cabe <jcabe4@gmail.com>
2014-02-15 15:21:07 -0800

    Imported Deanna, Jonathan R, and Michael's stuff
    via packages

--- Assets/Scripts/Robinson/inventory.cs
+++ Assets/Scripts/Robinson/inventory.cs
@@ -1,14 +1,14 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/************************************************************
+ * Script: Inventory.cs
+ * Author: Jonathan Robinson
+ * Description: Inventory container class.
+ ************************************************************/
 
-//This is a sample inventory script based on Unity Stealth tutorial.
+using UnityEngine;
+using System.Collections;
 
 public class Inventory : MonoBehaviour 
 {
-	public bool hasKey;  //Does player have a key?
+	public bool hasKey;  
 	public bool hasBook;
 }
-
-//This script needs to be applied to the players via drag and drop.
-
-//Source: www.youtube.com/watch?v=PznzVRjQztc#t=286 
