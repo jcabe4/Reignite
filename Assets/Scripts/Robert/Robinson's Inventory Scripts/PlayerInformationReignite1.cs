2c7b76f0c8612fa255ecf1514e0a86bd81386814
Jonathan Cabe <jcabe4@gmail.com>
2014-02-15 17:05:05 -0800

    Commented Out Redundant Code

--- Assets/Scripts/Robert/Robinson's Inventory Scripts/PlayerInformationReignite1.cs
+++ Assets/Scripts/Robert/Robinson's Inventory Scripts/PlayerInformationReignite1.cs
@@ -1,151 +1,151 @@
-/********Reignite
- * Script: PlayerInformation.cs
- * Author: Jonathan Robinson
- * Updated: 2.12.2014
- * Description: This script holds all relevant data
- * associated with the player as well as mutator and
- * accessor functions.  This script also loads and saves
- * these values from/into an xml. Based on work from Jonathan Cabe.
- * ******************************************************/
-
-
-using UnityEngine;
-using System.IO;
-using System.Xml;
-using System.Xml.Linq;
-using System.Collections;
-using System.Collections.Generic;
-
-
-public class PlayerInformationReignite : MonoBehaviour 
-{
-	public bool hasKey = false;
-	public bool hasBook = false;
-	private Inventory inventory; 
-	private GameObject Player;  
-
-
-	public static PlayerInformationReignite Instance
-	{
-		get
-		{
-			return instance;
-		}
-	}
-
-
-	private static PlayerInformationReignite instance;
-#if UNITY_EDITOR || UNITY_PC 	
-	private string savePath = "Assets/Resources/Player Data/Info.xml";
-	private string resourcePath = "Player Data/Info";
-#endif
-
-
-	void Awake()
-	{				
-		Player = GameObject.FindGameObjectWithTag("Player");  
-		inventory = Player.GetComponent<Inventory>();  
-		Initialize();
-		instance = this;
-		DontDestroyOnLoad(gameObject);
-
-
-		if (File.Exists(savePath))
-		{
-			LoadData();
-		}
-		else
-		{			
-			SaveData();
-		}
-	}
-
-
-
-
-	public bool GetKey()
-	{
-		return hasKey;
-	}
-
-
-	public bool GetBook()
-	{
-		return hasBook;
-	}
-
-
-	public void Initialize()
-	{
-		hasKey = false;
-		hasBook = false;
-	}
-
-
-	public void SaveData()
-	{
-		Debug.Log ("Saving...");
-
-
-		if (File.Exists (savePath))
-		    {
-				File.Delete (savePath);
-			}
-		XmlWriter writer = new XmlTextWriter(savePath, System.Text.Encoding.UTF8);
-
-
-		writer.WriteStartDocument();
-		writer.WriteWhitespace("\n");
-		writer.WriteStartElement("Root");
-		writer.WriteWhitespace("\n\t");
-		writer.WriteElementString("hasKey", inventory.hasKey.ToString());
-		writer.WriteWhitespace("\n\t");
-		writer.WriteElementString("hasBook", inventory.hasBook.ToString());
-		writer.WriteEndElement();
-		writer.WriteEndDocument ();
-		writer.Close();
-
-
-			//XMLFileManager.EncryptFile(savePath);
-	}
-
-
-	public void LoadData()
-	{
-		Debug.Log("Loading...");
-		if (!File.Exists(savePath))
-		{
-			return;
-		}
-		else
-		{
-			//XMLFileManager.DecryptFile(savePath);
-		}
-
-
-		TextAsset asset = (TextAsset)Resources.Load (resourcePath);
-		XmlReader reader = XmlReader.Create (new StringReader (asset.text));
-
-
-		while (reader.Read())
-		{
-			if (reader.IsStartElement() && reader.NodeType == XmlNodeType.Element)
-			{
-				switch(reader.Name)
-				{
-					case "hasKey": inventory.hasKey = bool.Parse(reader.ReadElementString());
-						break;
-					case "hasBook": inventory.hasBook = bool.Parse(reader.ReadElementString());
-						break;
-					default:
-						break;
-				}
-			}
-		}
-		reader.Close();
-
-
-		//XMLFileManager.EncryptFile(savePath);
-	}
-}
-
+///********Reignite
+// * Script: PlayerInformation.cs
+// * Author: Jonathan Robinson
+// * Updated: 2.12.2014
+// * Description: This script holds all relevant data
+// * associated with the player as well as mutator and
+// * accessor functions.  This script also loads and saves
+// * these values from/into an xml. Based on work from Jonathan Cabe.
+// * ******************************************************/
+//
+//
+//using UnityEngine;
+//using System.IO;
+//using System.Xml;
+//using System.Xml.Linq;
+//using System.Collections;
+//using System.Collections.Generic;
+//
+//
+//public class PlayerInformationReignite : MonoBehaviour 
+//{
+//	public bool hasKey = false;
+//	public bool hasBook = false;
+//	private Inventory inventory; 
+//	private GameObject Player;  
+//
+//
+//	public static PlayerInformationReignite Instance
+//	{
+//		get
+//		{
+//			return instance;
+//		}
+//	}
+//
+//
+//	private static PlayerInformationReignite instance;
+//#if UNITY_EDITOR || UNITY_PC 	
+//	private string savePath = "Assets/Resources/Player Data/Info.xml";
+//	private string resourcePath = "Player Data/Info";
+//#endif
+//
+//
+//	void Awake()
+//	{				
+//		Player = GameObject.FindGameObjectWithTag("Player");  
+//		inventory = Player.GetComponent<Inventory>();  
+//		Initialize();
+//		instance = this;
+//		DontDestroyOnLoad(gameObject);
+//
+//
+//		if (File.Exists(savePath))
+//		{
+//			LoadData();
+//		}
+//		else
+//		{			
+//			SaveData();
+//		}
+//	}
+//
+//
+//
+//
+//	public bool GetKey()
+//	{
+//		return hasKey;
+//	}
+//
+//
+//	public bool GetBook()
+//	{
+//		return hasBook;
+//	}
+//
+//
+//	public void Initialize()
+//	{
+//		hasKey = false;
+//		hasBook = false;
+//	}
+//
+//
+//	public void SaveData()
+//	{
+//		Debug.Log ("Saving...");
+//
+//
+//		if (File.Exists (savePath))
+//		    {
+//				File.Delete (savePath);
+//			}
+//		XmlWriter writer = new XmlTextWriter(savePath, System.Text.Encoding.UTF8);
+//
+//
+//		writer.WriteStartDocument();
+//		writer.WriteWhitespace("\n");
+//		writer.WriteStartElement("Root");
+//		writer.WriteWhitespace("\n\t");
+//		writer.WriteElementString("hasKey", inventory.hasKey.ToString());
+//		writer.WriteWhitespace("\n\t");
+//		writer.WriteElementString("hasBook", inventory.hasBook.ToString());
+//		writer.WriteEndElement();
+//		writer.WriteEndDocument ();
+//		writer.Close();
+//
+//
+//			//XMLFileManager.EncryptFile(savePath);
+//	}
+//
+//
+//	public void LoadData()
+//	{
+//		Debug.Log("Loading...");
+//		if (!File.Exists(savePath))
+//		{
+//			return;
+//		}
+//		else
+//		{
+//			//XMLFileManager.DecryptFile(savePath);
+//		}
+//
+//
+//		TextAsset asset = (TextAsset)Resources.Load (resourcePath);
+//		XmlReader reader = XmlReader.Create (new StringReader (asset.text));
+//
+//
+//		while (reader.Read())
+//		{
+//			if (reader.IsStartElement() && reader.NodeType == XmlNodeType.Element)
+//			{
+//				switch(reader.Name)
+//				{
+//					case "hasKey": inventory.hasKey = bool.Parse(reader.ReadElementString());
+//						break;
+//					case "hasBook": inventory.hasBook = bool.Parse(reader.ReadElementString());
+//						break;
+//					default:
+//						break;
+//				}
+//			}
+//		}
+//		reader.Close();
+//
+//
+//		//XMLFileManager.EncryptFile(savePath);
+//	}
+//}
+//
--- Assets/Scripts/Robert/Robinson's Inventory Scripts/XMLFileManager1.cs
+++ Assets/Scripts/Robert/Robinson's Inventory Scripts/XMLFileManager1.cs
@@ -1,83 +1,83 @@
-/************************************************************
- * Script: XMLFileManager.cs
- * Author: Jonathan Robinson
- * Date:2.12.2014
- * Description: Both Encrypt and Decrypt Algorythms are
- * taken and slightly modified from
- * http://unitynoobs.blogspot.com/2012/01/xml-encrypting.html
- * Based on work from Jonathan Cabe
- ************************************************************/
-
-
-using UnityEngine;
-using System;
-using System.IO;
-using System.Xml;
-using System.Text;
-using System.Security.Cryptography;
-
-
-[ExecuteInEditMode]
-public class XMLFileManager : MonoBehaviour 
-{
-	public static void EncryptFile (string path)
-	{
-		if (!File.Exists(path))
-		{
-			Debug.Log("File Not Found At: " + path);
-			return;
-		}
-
-
-		XmlDocument xmlFile = new XmlDocument();
-		xmlFile.Load(path);
-		XmlElement xmlRoot = xmlFile.DocumentElement;
-
-
-		if (xmlRoot.ChildNodes.Count > 1)
-		{
-			byte[] keyArray = UTF8Encoding.UTF8.GetBytes ("86759426197648123460789546213421");
-			byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes (xmlRoot.InnerXml);
-			RijndaelManaged rDel = new RijndaelManaged();
-			rDel.Key = keyArray;
-			rDel.Mode = CipherMode.ECB;
-			rDel.Padding = PaddingMode.PKCS7;
-			ICryptoTransform cTransform = rDel.CreateEncryptor();
-			byte[] resultArray = cTransform.TransformFinalBlock (toEncryptArray, 0, toEncryptArray.Length);
-			xmlRoot.InnerXml = Convert.ToBase64String (resultArray, 0, resultArray.Length);
-		}
-
-
-		xmlFile.Save(path);
-	}
-
-
-	public static void DecryptFile (string path)
-	{
-		if (!File.Exists(path))
-		{
-			Debug.Log("File Not Found At: " + path);
-			return;
-		}
-
-
-		XmlDocument xmlFile = new XmlDocument();
-		xmlFile.Load(path);
-		XmlElement xmlRoot = xmlFile.DocumentElement;
-
-
-		if (xmlRoot.ChildNodes.Count <= 1)
-		{
-			byte[] keyArray = UTF8Encoding.UTF8.GetBytes ("86759426197648123460789546213421");
-			byte[] toEncryptArray = Convert.FromBase64String (xmlRoot.InnerXml);
-			RijndaelManaged rDel = new RijndaelManaged();
-			rDel.Key = keyArray;
-			rDel.Mode = CipherMode.ECB;
-			rDel.Padding = PaddingMode.PKCS7;
-			ICryptoTransform cTransform = rDel.CreateDecryptor();
-			byte[] resultArray = cTransform.TransformFinalBlock (toEncryptArray, 0, toEncryptArray.Length);
-			xmlRoot.InnerXml = UTF8Encoding.UTF8.GetString (resultArray);
-		}
-	}
-}
-
+///************************************************************
+// * Script: XMLFileManager.cs
+// * Author: Jonathan Robinson
+// * Date:2.12.2014
+// * Description: Both Encrypt and Decrypt Algorythms are
+// * taken and slightly modified from
+// * http://unitynoobs.blogspot.com/2012/01/xml-encrypting.html
+// * Based on work from Jonathan Cabe
+// ************************************************************/
+//
+//
+//using UnityEngine;
+//using System;
+//using System.IO;
+//using System.Xml;
+//using System.Text;
+//using System.Security.Cryptography;
+//
+//
+//[ExecuteInEditMode]
+//public class XMLFileManager : MonoBehaviour 
+//{
+//	public static void EncryptFile (string path)
+//	{
+//		if (!File.Exists(path))
+//		{
+//			Debug.Log("File Not Found At: " + path);
+//			return;
+//		}
+//
+//
+//		XmlDocument xmlFile = new XmlDocument();
+//		xmlFile.Load(path);
+//		XmlElement xmlRoot = xmlFile.DocumentElement;
+//
+//
+//		if (xmlRoot.ChildNodes.Count > 1)
+//		{
+//			byte[] keyArray = UTF8Encoding.UTF8.GetBytes ("86759426197648123460789546213421");
+//			byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes (xmlRoot.InnerXml);
+//			RijndaelManaged rDel = new RijndaelManaged();
+//			rDel.Key = keyArray;
+//			rDel.Mode = CipherMode.ECB;
+//			rDel.Padding = PaddingMode.PKCS7;
+//			ICryptoTransform cTransform = rDel.CreateEncryptor();
+//			byte[] resultArray = cTransform.TransformFinalBlock (toEncryptArray, 0, toEncryptArray.Length);
+//			xmlRoot.InnerXml = Convert.ToBase64String (resultArray, 0, resultArray.Length);
+//		}
+//
+//
+//		xmlFile.Save(path);
+//	}
+//
+//
+//	public static void DecryptFile (string path)
+//	{
+//		if (!File.Exists(path))
+//		{
+//			Debug.Log("File Not Found At: " + path);
+//			return;
+//		}
+//
+//
+//		XmlDocument xmlFile = new XmlDocument();
+//		xmlFile.Load(path);
+//		XmlElement xmlRoot = xmlFile.DocumentElement;
+//
+//
+//		if (xmlRoot.ChildNodes.Count <= 1)
+//		{
+//			byte[] keyArray = UTF8Encoding.UTF8.GetBytes ("86759426197648123460789546213421");
+//			byte[] toEncryptArray = Convert.FromBase64String (xmlRoot.InnerXml);
+//			RijndaelManaged rDel = new RijndaelManaged();
+//			rDel.Key = keyArray;
+//			rDel.Mode = CipherMode.ECB;
+//			rDel.Padding = PaddingMode.PKCS7;
+//			ICryptoTransform cTransform = rDel.CreateDecryptor();
+//			byte[] resultArray = cTransform.TransformFinalBlock (toEncryptArray, 0, toEncryptArray.Length);
+//			xmlRoot.InnerXml = UTF8Encoding.UTF8.GetString (resultArray);
+//		}
+//	}
+//}
+//
