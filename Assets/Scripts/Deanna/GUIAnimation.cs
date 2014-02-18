600b320748e87016a57b7eea379a4f1fae345a62
mcswedo <mcswedo@Michaels-MacBook-Pro.local>
2014-02-17 23:17:33 -0800

    Implemented flame animation with a generic GUIAnimation script, and a Grabber script to get animations and attach to anchors.
    POTENTIAL GITHUB BREAK B/C CONFLICTS WITH TITLE SCREEN SCENE

---
+++ Assets/Scripts/Deanna/GUIAnimation.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIAnimation : MonoBehaviour 
+{
+	public int numberOfFrames;
+	public string prefix;
+
+	private UISprite currentSprite;
+
+	private int index = 1;
+	private int frameCount = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		currentSprite = gameObject.GetComponent<UISprite>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		frameCount++;
+		
+		if(frameCount >= 8)
+		{
+			if(index < numberOfFrames)
+			{
+				index++;
+				currentSprite.spriteName = prefix + index.ToString();
+			}
+			else
+			{
+				index = 1;
+				currentSprite.spriteName = prefix + index.ToString();
+			}
+
+			frameCount = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/Deanna/MenuButton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour 
+{
+	public enum ButtonType {NewGame, LoadGame, Practice, Options, Exit}
+	public ButtonType type;
+	
+	void OnClick()
+	{
+		switch (type)
+		{
+		case ButtonType.NewGame:
+		{
+			Application.LoadLevel("Library 2F");
+			break;
+		}
+		case ButtonType.LoadGame:
+		{
+			break;
+		}
+		case ButtonType.Practice:
+		{
+			Application.LoadLevel("Rhythm Section");
+			break;
+		}
+		case ButtonType.Options:
+		{
+			break;
+		}
+		case ButtonType.Exit:
+		{
+			Application.Quit();
+			break;
+		}
+		default:
+		{
+			break;
+		}
+		}
+	}
+}
