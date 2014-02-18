11804778c4a1d686b58b237e518a582051c8f9ea
jcabe4 <jcabe4@gmail.com>
2014-02-17 16:37:51 -0800

    Added Music On Main Screen
    And Some Comments

---
+++ Assets/Scripts/Jonathan Cabe/MusicController.cs
@@ -0,0 +1,47 @@
+﻿/*****************************************************
+ * Program: Reignite
+ * Script: MusicController.cs
+ * Author: Jonathan Cabe
+ * Description: This script will control environment
+ * music depending on which scene you are in.  For now
+ * it mostly ensures music flows uninterrupted.
+ * ***************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour 
+{
+	public static MusicController Instance
+	{
+		get
+		{
+			return instance;
+		}
+	}
+
+	private static MusicController instance;
+
+	void Start()
+	{
+		if (MusicController.Instance && MusicController.Instance != this)
+		{
+			Destroy(gameObject);
+		}
+		else if (!MusicController.Instance)
+		{
+			instance = this;
+		}
+
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void OnLevelWasLoaded()
+	{
+		Debug.Log (Application.loadedLevelName);
+		if (Application.loadedLevelName == "Rhythm Section")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
