85d1bea7c606d0203e285ec4398d86167163b6e6
Jonathan Robinson <robij308@coyote.csusb.edu>
2014-03-05 20:41:59 -0800

    Inventory system ready

---
+++ Assets/Scripts/Robinson/Item.cs
@@ -0,0 +1,19 @@
+﻿/**************************************************************************
+ * Item.cs                                                                *
+ * Item class for PlayerInformationReignite.cs and Pickup.cs              *
+ * BY:Jonathan Robinson and Robert Rojas                                  *
+ * 3.5.2014                                                               *
+ * ************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item : MonoBehaviour
+{
+	public string itemName;
+	public bool contains;
+	public string itemDescription;
+	public int itemID;
+}
+
